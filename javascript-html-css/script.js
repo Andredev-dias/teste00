cf8861f0056a74c2378786aa59b511836b8f3835
Andredev-dias <andredias.curitiba@gmail.com>
2024-02-05 17:18:11 -0300

    media do kleber

--- javascript-html-css/script.js
+++ javascript-html-css/script.js
@@ -4,3 +4,15 @@ function soma(){
     var res = num1 + num2;
     document.getElementById("resSoma").textContent = res.toFixed(2);
 }
+
+function media(){
+    var media1 = document.getElementById("media1").valueAsNumber;
+    var media2 = document.getElementById("media2").valueAsNumber;
+    var media3 = document.getElementById("media3").valueAsNumber;
+    var resMedia = (media1 + media2 + media3) / 3;
+    document.getElementById("resMedia").textContent = resMedia.toFixed(2);
+}
+
+function limpar(){
+    window.location.reload();
+}
